d2e46488005170b6bbcdb3396c95e493c78c98cb
Paul Shryock <pshryock@nba.com>
2022-06-09 20:28:01 -0400

    Do not minify component library

--- dist/js/components.js
+++ dist/js/components.js
@@ -1,1 +1,65 @@
-var y=Object.defineProperty;var e=Object.getOwnPropertySymbols;var p=Object.prototype.hasOwnProperty,s=Object.prototype.propertyIsEnumerable;var i=(t,n,r)=>n in t?y(t,n,{enumerable:!0,configurable:!0,writable:!0,value:r}):t[n]=r,o=(t,n)=>{for(var r in n||(n={}))p.call(n,r)&&i(t,r,n[r]);if(e)for(var r of e(n))s.call(n,r)&&i(t,r,n[r]);return t};var B=(t=>typeof require!="undefined"?require:typeof Proxy!="undefined"?new Proxy(t,{get:(n,r)=>(typeof require!="undefined"?require:n)[r]}):t)(function(t){if(typeof require!="undefined")return require.apply(this,arguments);throw new Error('Dynamic require of "'+t+'" is not supported')});var b=(t,n)=>{var r={};for(var u in t)p.call(t,u)&&n.indexOf(u)<0&&(r[u]=t[u]);if(t!=null&&e)for(var u of e(t))n.indexOf(u)<0&&s.call(t,u)&&(r[u]=t[u]);return r};var c=(t,n)=>()=>(n||t((n={exports:{}}).exports,n),n.exports);var q=c((k,f)=>{var d=B("react");function g(r){var u=r,{label:t}=u,n=b(u,["label"]);return t?d.createElement("button",o({type:"button"},n),t):null}f.exports={Button:g}});var h=c((l,x)=>{x.exports=o({},q())});export default h();
+var __defProp = Object.defineProperty;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getOwnPropSymbols = Object.getOwnPropertySymbols;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __propIsEnum = Object.prototype.propertyIsEnumerable;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __spreadValues = (a, b) => {
+  for (var prop in b || (b = {}))
+    if (__hasOwnProp.call(b, prop))
+      __defNormalProp(a, prop, b[prop]);
+  if (__getOwnPropSymbols)
+    for (var prop of __getOwnPropSymbols(b)) {
+      if (__propIsEnum.call(b, prop))
+        __defNormalProp(a, prop, b[prop]);
+    }
+  return a;
+};
+var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
+  get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
+}) : x)(function(x) {
+  if (typeof require !== "undefined")
+    return require.apply(this, arguments);
+  throw new Error('Dynamic require of "' + x + '" is not supported');
+});
+var __objRest = (source, exclude) => {
+  var target = {};
+  for (var prop in source)
+    if (__hasOwnProp.call(source, prop) && exclude.indexOf(prop) < 0)
+      target[prop] = source[prop];
+  if (source != null && __getOwnPropSymbols)
+    for (var prop of __getOwnPropSymbols(source)) {
+      if (exclude.indexOf(prop) < 0 && __propIsEnum.call(source, prop))
+        target[prop] = source[prop];
+    }
+  return target;
+};
+var __commonJS = (cb, mod) => function __require2() {
+  return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+};
+
+// src/components/Button.js
+var require_Button = __commonJS({
+  "src/components/Button.js"(exports, module) {
+    var React = __require("react");
+    function Button(_a) {
+      var _b = _a, { label } = _b, props = __objRest(_b, ["label"]);
+      if (!label)
+        return null;
+      return /* @__PURE__ */ React.createElement("button", __spreadValues({
+        type: "button"
+      }, props), label);
+    }
+    module.exports = {
+      Button
+    };
+  }
+});
+
+// src/components.js
+var require_components = __commonJS({
+  "src/components.js"(exports, module) {
+    module.exports = __spreadValues({}, require_Button());
+  }
+});
+export default require_components();
